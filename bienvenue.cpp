98bd0ff48df1d7d98f94121e03100a24dbe2c1b7
Waelyacoub <waelyacooub@gmail.com>
2024-02-26 13:07:32 +0100

    Modification de la fonction afficherBienvenue() pour accepter un argument supplémentaire

--- bienvenue.cpp
+++ bienvenue.cpp
@@ -1,7 +1,27 @@
-// Affiche un message de bienvenue
 #include "fonction-bienvenue.h"
-int main()
-{
-afficherBienvenue();
-return 0;
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    std::string message = "Bienvenue le monde !"; // Message par défaut
+    int nbAffichage = 1; // Nombre par défaut de fois que le message doit être affiché
+
+    // Si des arguments de la ligne de commande sont fournis
+    if (argc > 1) {
+        // Le premier argument est le message de bienvenue à afficher
+        message = argv[1];
+        
+        // Si un deuxième argument est fourni, c'est le nombre de fois que le message doit être affiché
+        if (argc > 2) {
+            nbAffichage = std::stoi(argv[2]); // Convertit l'argument en entier
+        }
+    }
+
+    // Affiche le message de bienvenue
+    for (int i = 0; i < nbAffichage; ++i) {
+        afficherBienvenue(message);
+    }
+
+    return 0;
 }
+
